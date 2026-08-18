15ad46230c8e46848025ef2ca2c888acb660f8c5
kavidu284 <wijenayakakavidu@gmail.com>
2026-08-18 19:16:40 +0530

    10.6

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,6 +18,7 @@ from app.routes.round_robin import router as round_robin_router
 from app.routes.result import router as result_router
 from app.routes.administration import router as administration_router
 from app.routes.AdministratorLogin import router as admin_login_router
+from app.routes.shuffle import router as shuffle_router
 
 
 
@@ -49,6 +50,7 @@ app.include_router(round_robin_router)
 app.include_router(result_router)
 app.include_router(administration_router)
 app.include_router(admin_login_router)
+app.include_router(shuffle_router)
 
 
 @app.get("/")
